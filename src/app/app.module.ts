d5c94897c9030c202ed0400dbbbe2274ce81c7d0
u4saif <u4saif@gmail.com>
2020-09-11 00:01:04 +0530

    commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CreateCouponComponent } from './create-coupon/create-coupon.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from '@angular/material/core';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateCouponComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/create-coupon/create-coupon.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-coupon',
+  templateUrl: './create-coupon.component.html',
+  styleUrls: ['./create-coupon.component.css']
+})
+export class CreateCouponComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  check(f){
+    console.log(f.value);
+   }
+
+}
